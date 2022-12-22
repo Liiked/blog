aefb3f0ebcccda30c01c7c4c8cc704d1d8b1bc75
liik <linyuanqianpp@126.com>
2022-12-22 11:51:28 +0800

    feat: 配置初始化

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,17 @@
+export default {
+  title: "Square Coin",
+  description: "100块都没有的博客",
+  themeConfig: {
+    sidebar: [
+      {
+        text: "前端",
+        items: [
+          {
+            text: "比较 useRequest 看组件设计",
+            link: "/fe/component-design-by-compare-with-request",
+          },
+        ],
+      },
+    ],
+  },
+};
